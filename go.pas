87df7cccc3fb40a98f258bb13bd76cf5ad792e82
Matthew Aitchison <matthewsaitchison@gmail.com>
2024-12-06 11:18:48 +1300

    fix

--- go.pas
+++ go.pas
@@ -46,8 +46,8 @@ begin
   {$I-}
   mkDIR(destinationPath);
   {$I+}
-  dos.exec(getEnv('COMSPEC'), '/C rmdir /s '+destinationPath+'_');
-  dos.exec(getEnv('COMSPEC'), '/C move '+destinationPath+' '+destinationPath+'_');
+  dos.exec(getEnv('COMSPEC'), '/C rmdir /s '+destinationPath+'_tmp');
+  dos.exec(getEnv('COMSPEC'), '/C move '+destinationPath+' '+destinationPath+'_tmp');
   dos.exec(getEnv('COMSPEC'), '/C copy *.pas '+destinationPath);
 end;
 
