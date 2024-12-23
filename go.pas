0ea2c22369cbb005ec415ec87c757dca6d73fbec
Matthew Aitchison <matthewsaitchison@gmail.com>
2024-12-23 13:30:20 +1300

    fixed no daily folder bug

--- go.pas
+++ go.pas
@@ -79,7 +79,11 @@ begin
   close(t);
 
   {it's handy to have a daily folder aswell}
-  safeCopy('$rep\'+time.YYMMDD(''));
+  destinationPath := '$rep\'+time.YYMMDD('')
+  {$I-}
+  mkDIR(destinationPath);
+  {$I+}
+  safeCopy(destinationPath);
 
   {and head...}
   safeCopy('$rep\head');
