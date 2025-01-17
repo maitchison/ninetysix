3acb158a5f0653c7d012310827af56d2d74e0e49
Matthew Aitchison <matthewsaitchison@gmail.com>
2025-01-18 12:12:55 +1300

    added build script for go

---
+++ go/build.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set BIN_DIR=c:\dev\bin
+
+echo =======================
+echo Building: debug
+echo =======================
+del go.exe
+fpc @fp.cfg -dDEBUG -B -v0 go.pas
+IF ERRORLEVEL 1 goto :buildError
+
+copy tmp\go.exe %BIN_DIR%\god.exe
+:deltree /y tmp
+
+echo =======================
+echo Building: normal
+echo =======================
+del go.exe
+fpc @fp.cfg -dNORMAL -B -v0 go.pas
+IF ERRORLEVEL 1 goto :buildError
+
+copy go.exe %BIN_DIR%\go.exe
+
+:buildError
+
+echo.
+echo Build failed!
+echo.
+goto :end
+
+:end
