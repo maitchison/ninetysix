d28e281ccae5e7a99006e11c601053418cac6a2f
Matthew Aitchison <matthewsaitchison@gmail.com>
2025-02-23 23:29:03 +1300

    more optimizations (less than 100ms now)

--- destruct/cfd.pas
+++ destruct/cfd.pas
@@ -36,8 +36,8 @@ type
     size: tPowerOfTwo;
     procedure init();
     procedure setDensity(x,y: integer; value: integer);
-    procedure computeMacros(var rho, ux, uy: single; x,y: integer);
-    function  freq(i: integer; rho,ux,uy: single): single;
+    procedure computeMacros(var rho, ux, uy: single; x,y: integer); inline;
+    function  calcFreq(i: integer; rho,ux,uy,uxuv: single): single; inline;
     procedure collision(force: boolean=false);
     procedure stream();
     procedure update();
@@ -45,7 +45,7 @@ type
   end;
 
 
-var
+const
   w: array[0..8] of single = (4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36);
   cx: array[0..8] of integer = (0, +1,  0, -1,  0, +1, -1, -1, +1);
   cy: array[0..8] of integer = (0,  0, +1,  0, -1, +1, +1, -1, -1);
@@ -102,64 +102,65 @@ end;
 procedure tCFDGrid.computeMacros(var rho, ux, uy: single; x,y: integer);
 var
   i: integer;
+  a: array[0..8] of single;
 begin
-  rho := 0;
-  ux := 0;
-  uy := 0;
-  for i := 0 to 8 do
-    rho += f[i,x,y];
-  if rho > 1e-6 then begin
-    {this could be much optimized with MMX}
-    for i := 0 to 8 do begin
-      ux += f[i,x,y]*cx[i];
-      uy += f[i,x,y]*cy[i];
-    end;
-    ux := ux / rho;
-    uy := uy / rho;
-  end else begin
-    ux := 0;
-    uy := 0;
+  for i := 0 to 8 do a[i] := f[i,x,y];
+  rho := a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8];
+  if rho < 1e-6 then begin
+    ux := 0; uy := 0;
+    exit;
   end;
+  ux := (a[1] - a[3] + a[5] - a[6] - a[7] + a[8]) / rho;
+  uy := (a[2] - a[4] + a[5] + a[6] - a[7] - a[8]) / rho;
 end;
 
-function tCFDGrid.freq(i: integer; rho,ux,uy: single): single;
+function tCFDGrid.calcFreq(i: integer; rho,ux,uy,uxuv: single): single; inline;
 var
-  eu, uv: single;
+  eu: single;
 begin
   eu := cx[i]*ux + cy[i]*uy;
-  uv := sqr(ux) + sqr(uy);
-  freq := w[i] * rho * (1.0 + 3.0 * eu + 4.5 * eu * eu - 1.5 * uv);
+  result := w[i] * rho * (1.0 + 3.0 * eu + 4.5 * eu * eu - 1.5 * uxuv);
 end;
 
 procedure tCFDGrid.collision(force: boolean=false);
 var
   x,y,i: integer;
-  gx,gy: integer;
-
-  procedure cellSlow(gx,gy: integer);
-  var
-    xx,yy,i,x,y: integer;
-    rho, ux, uy, freqVal: single;
-  begin
-    for yy := 0 to 7 do begin
-      for xx := 0 to 7 do begin
-        x := gx * 8 + xx;
-        y := gy * 8 + yy;
-        computeMacros(rho, ux, uy, x, y);
-        for i := 0 to 8 do begin
-          freqVal := freq(i, rho, ux, uy);
-          fTemp[i,x,y] := f[i,x,y] + omega * (freqVal - f[i,x,y]);
-        end;
-        displayRho[x,y] := rho;
-        displayVel[x,y] := sqr(ux) + sqr(uy);
-      end;
-    end;
-  end;
+  rho, ux, uy, freq: single;
+  uxuv,uv15: single;
+  eu: single;
 
 begin
-  for gy := 0 to 15 do begin
-    for gx := 0 to 15 do begin
-      cellSlow(gx, gy);
+  for y := 0 to 127 do begin
+    for x := 0 to 127 do begin
+      computeMacros(rho, ux, uy, x, y);
+      uxuv := sqr(ux) + sqr(uy);
+
+      for i := 3 to 8 do begin
+        freq := calcFreq(i, rho, ux, uy, uxuv);
+        fTemp[i,x,y] := f[i,x,y] + omega * (freq - f[i,x,y]);
+      end;
+
+      uv15 := uxuv * 1.5;
+
+      eu := 0;
+      freq := (4/9) * rho * (1.0 - uv15);
+      fTemp[0,x,y] := f[0,x,y] + omega * (freq - f[0,x,y]);
+      eu := ux;
+      freq := (1/9) * rho * (1.0 + 3.0 * eu + 4.5 * eu * eu - uv15);
+      fTemp[1,x,y] := f[1,x,y] + omega * (freq - f[1,x,y]);
+      eu := uy;
+      freq := (1/9) * rho * (1.0 + 3.0 * eu + 4.5 * eu * eu - uv15);
+      fTemp[2,x,y] := f[2,x,y] + omega * (freq - f[2,x,y]);
+
+
+{    cx: array[0..8] of integer = (0, +1,  0, -1,  0, +1, -1, -1, +1);
+    cy: array[0..8] of integer = (0,  0, +1,  0, -1, +1, +1, -1, -1);
+ }{
+      eu := cx[i]*ux + cy[i]*uy;
+      freq := w[i] * rho * (1.0 + 3.0 * eu + 4.5 * eu * eu - 1.5 * uxuv);
+   }
+      displayRho[x,y] := rho;
+      displayVel[x,y] := uxuv;
     end;
   end;
 end;
