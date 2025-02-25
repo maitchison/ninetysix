e9a75d53cecb2ce0b73017c6b403fefca88e666e
Matthew Aitchison <matthewsaitchison@gmail.com>
2025-02-26 12:11:06 +1300

    implement 1d templates, but they don't make sense so switch back to 2d templates

--- destruct/template.pas
+++ destruct/template.pas
@@ -13,17 +13,25 @@ scale too much above 32 (I think).
 interface
 
 uses
+  debug,
+  test,
+  utils,
   graph32;
 
 procedure drawTemplateAdd(dst: tPage; x,y: integer; radius: word; col: RGBA);
 
 implementation
 
+var
+  {returns F(sqr(16*x)) * 255; for x=0..1}
+  LOOKUP: array[0..1, 0..255] of byte;
+
 procedure drawTemplateAdd_REF(dst: tPage; x,y: integer; radius: word; col: RGBA);
 var
   dx,dy: integer;
   r2, d2: integer;
   c: RGBA;
+  f: integer;
   v: single;
 begin
   if radius <= 0 then exit;
@@ -34,7 +42,9 @@ begin
     for dx := -radius to +radius do begin
       d2 := dy*dy+dx*dx;
       if d2 >= r2 then continue;
-      v := (1-(sqrt(d2) / radius)) * (col.a/255);
+      //v := (1-(sqrt(d2) / radius)) * (col.a/255);
+      f := clamp(round((d2/r2)*16 * (col.a/255)), 0, 255);
+      v := LOOKUP[0, f] / 255;
       c := dst.getPixel(x+dx, y+dy);
       c.init(
         round(c.r + col.r*v),
@@ -51,5 +61,21 @@ begin
   drawTemplateAdd_REF(dst, x, y, radius, col);
 end;
 
+procedure generateLookups();
+var
+  i: integer;
+  x: single;
+  f: single;
 begin
+  for i := 0 to 255 do begin
+    x := (i/255);
+    f := 1-x;
+    // quadratic falloff
+    LOOKUP[0, i] := clamp(round(sqr(16*f)), 0, 255);
+    note(intToStr(LOOKUP[0, i]));
+  end;
+end;
+
+begin
+  generateLookups();
 end.
