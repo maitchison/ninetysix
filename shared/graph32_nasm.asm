f6b6d2655999046eca85c63c6ac47af531b86e6a
Matthew Aitchison <matthewsaitchison@gmail.com>
2025-03-15 11:00:22 +1300

    nasm code for stretch draw

--- shared/graph32_nasm.asm
+++ shared/graph32_nasm.asm
@@ -1,14 +1,18 @@
-; drawLine_MMX NASM implementation
-; esi: srcPtr -> moved to end of line
-; edi: dstPtr -> moved to end of line
-; mm1: tintColor
-; mm3: bias (255 as 4x uint16)
-; eax -> destroyed
-; ebx: [preserved]
-; ecx: pixel count -> destroyed
-
 section .text
 [BITS 32]
+
+; -------------------------------
+; Register Conventions
+; -------------------------------
+; esi: srcPtr [moved to end of line]
+; edi: dstPtr [moved to end of line]
+; mm0: 0
+; mm1: [used for current color]
+; mm2: tint
+; mm3: bias (255 as 4x uint16)
+; eax: [destroyed]
+; ebx: [preserved]
+; ecx: pixel count [0]
 
 
 GLOBAL _drawLine_MMX
@@ -16,19 +20,32 @@ GLOBAL _drawLine_Tint_Blend_MMX
 GLOBAL _drawLine_Tint_MMX
 GLOBAL _drawLine_Blend_MMX
 
+GLOBAL _stretchLine_Tint_Blend_Nearest_MMX
+GLOBAL _stretchLine_Blend_Nearest_MMX:
+GLOBAL _stretchLine_Tint_Nearest_MMX:
+GLOBAL _stretchLine_Nearest_MMX:
+
+
+; common registers
+%define zer mm0
+%define src mm1
+%define tnt mm2
+%define b25 mm3
+
 %macro DRAW_SAMPLE 0
-    movd      mm2, [esi]
-    punpcklbw mm2, mm0
+    movd      src, [esi]
+    add       esi, 4
+    punpcklbw src, zer
 %endmacro
 
 %macro DRAW_TINT 0
-    pmullw    mm2, mm1
-    paddw     mm2, mm3
-    psrlw     mm2, 8
+    pmullw    src, tnt
+    paddw     src, b25
+    psrlw     src, 8
 %endmacro
 
 %macro DRAW_BLEND 0
-    movq      mm7, mm2
+    movq      mm7, src
     psrlq     mm7, 48
     movd      eax, mm7
     cmp       al, 0
@@ -37,35 +54,37 @@ GLOBAL _drawLine_Blend_MMX
     je        .blit
     
     movd      mm6, [edi]
-    punpcklbw mm6, mm0
+    punpcklbw mm6, zer
 
     movd      mm4, eax
     punpcklwd mm4, mm4
     punpckldq mm4, mm4
 
-    movq      mm5, mm3
+    movq      mm5, b25
     psubw     mm5, mm4
 
-    pmullw    mm2, mm4
+    pmullw    src, mm4
     pmullw    mm6, mm5
-    paddw     mm2, mm6
-    paddw     mm2, mm3
-    psrlw     mm2, 8
+    paddw     src, mm6
+    paddw     src, b25
+    psrlw     src, 8
 %endmacro
 
 %macro DRAW_BLIT 0
-    packuswb  mm2, mm2
-    movd      [edi], mm2
+    packuswb  src, src
+    movd      [edi], src
 %endmacro
 
-%macro DRAW_EOL 0
-    add edi, 4
-    add esi, 4
+%macro DRAW_END 0
+    add edi, 4    
     dec ecx
     jnz .xloop
 %endmacro
 
-; Main Procedure
+; -------------------------------------------------
+; DRAWING
+; -------------------------------------------------
+
 _drawLine_Tint_Blend_MMX:
 .xloop:  
   DRAW_SAMPLE
@@ -74,7 +93,7 @@ _drawLine_Tint_Blend_MMX:
 .blit:        
   DRAW_BLIT
 .skip:    
-  DRAW_EOL
+  DRAW_END
   ret
 
 _drawLine_Tint_MMX:
@@ -84,7 +103,7 @@ _drawLine_Tint_MMX:
 .blit:      
   DRAW_BLIT  
 .skip:    
-  DRAW_EOL  
+  DRAW_END  
   ret
 
 _drawLine_Blend_MMX:
@@ -94,9 +113,10 @@ _drawLine_Blend_MMX:
 .blit:      
   DRAW_BLIT    
 .skip:    
-  DRAW_EOL  
+  DRAW_END  
   ret
 
+; special case for direct blit.
 _drawLine_MMX:
   push ecx
   shr ecx, 2
@@ -116,3 +136,55 @@ _drawLine_MMX:
   and ecx, 3  
   rep movsd
   ret
+
+; -------------------------------------------------
+; SCALING
+; -------------------------------------------------
+
+; stretchLine_MMX NASM implementation
+; ebx: tx * 65536
+; edx: tdx * 65536
+
+%macro STRETCH_SAMPLE 0
+  mov       eax, ebx
+  shr       eax, 16
+  movd      src, [esi+eax*4]
+  punpcklbw src, zer
+  add       ebx, edx
+%endmacro
+
+_stretchLine_Tint_Blend_Nearest_MMX:
+ .xloop:
+  STRETCH_SAMPLE
+  DRAW_TINT
+  DRAW_BLEND
+.blit:        
+  DRAW_BLIT
+.skip:    
+  DRAW_END
+
+_stretchLine_Tint_Nearest_MMX:
+ .xloop:
+  STRETCH_SAMPLE
+  DRAW_TINT
+.blit:        
+  DRAW_BLIT
+.skip:    
+  DRAW_END  
+
+_stretchLine_Blend_Nearest_MMX:
+ .xloop:
+  STRETCH_SAMPLE
+  DRAW_BLEND  
+.blit:        
+  DRAW_BLIT
+.skip:    
+  DRAW_END  
+
+_stretchLine_Nearest_MMX:
+ .xloop:
+  STRETCH_SAMPLE
+.blit:        
+  DRAW_BLIT
+.skip:    
+  DRAW_END  
