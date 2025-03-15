91447f26d52cadc13fc386ac0c45c34e1cd8a7d4
Matthew Aitchison <matthewsaitchison@gmail.com>
2025-03-16 12:18:49 +1300

    updated to new master

--- destruct/master.pas
+++ destruct/master.pas
@@ -1,21 +1,40 @@
 
 program master;
 
+{
+todo: full auto mode
+- [ ] Detect file changes
+- [ ] Long audio gets auto ETA
+}
+
 uses
-  debug,
-  utils,
-  graph32,
+  uDebug,
+  uUtils,
   crt,
-  sound,
-  stream,
-  la96,
-  lc96,
-  sysPNG,
-  filesystem;
+  sysUtils,
+  uGraph32,
+  uSound,
+  uStream,
+  uLA96,
+  uLP96,
+  uPNG,
+  uIniFile,
+  uResLib,
+  uFilesystem;
 
 var
   {force processing of all files}
   FORCE: boolean = false;
+  resLib: tResourceLibrary;
+
+const
+  {TODO: from CWD}
+  SRC_ROOT = 'c:\masters\destruct';
+  DST_ROOT = 'c:\dev\destruct\';
+
+type
+  tConvertProc = procedure(srcPath, dstPath: string);
+  tFileType = (ftPNG, ftWAVE);
 
 procedure updateEncodeProgress(frameOn: int32; samplePtr: pAudioSample16S; frameLength: int32);
 begin
@@ -23,32 +42,64 @@ begin
   if frameOn mod 16 = 15 then write('.');
 end;
 
-procedure convertPNG(filename: string);
+{checks if we need to process file}
+function preProcess(srcFile, dstFile: string): boolean;
 var
-  srcPath, dstPath: string;
-  img: tPage;
+  tag: string;
+  needsUpdate: boolean;
 begin
-  srcPath := joinPath('c:\dev\masters\destruct', filename+'.png');
-  dstPath := 'res\'+filename+'.p96';
-  if (not FORCE) and fs.exists(dstPath) then exit;
-  img := tPage.Load(srcPath);
-  saveLC96(dstPath, img);
+  tag := extractFilename(srcFile);
+  if tag.startsWith('_') then exit(false);
+  textAttr := LightGray;
+  write(pad(tag, 40));
+  needsUpdate := resLib.needsUpdate(dstFile);
   textAttr := Green;
-  writeLn(format('[%dx%d]',[img.width, img.height]));
+  if not needsUpdate then writeln('[Skip]');
   textAttr := White;
-  img.free;
+  exit(needsUpdate);
 end;
 
-procedure convertWave(filename: string);
+procedure postProcess(srcFile, dstFile: string);
 var
-  srcPath, dstPath: string;
-  writer: tLA96Writer;
-  sfx: tSoundEffect;
+  res: tResourceInfo;
 begin
-  srcPath := joinPath('c:\dev\masters\destruct', filename+'.wav');
-  dstPath := 'res\'+filename+'.a96';
-  if (not FORCE) and fs.exists(dstPath) then exit;
-  sfx := tSoundEffect.Load(srcPath);
+  res.srcFile := srcFile;
+  res.dstFile := dstFile;
+  res.modifiedTime := fileSystem.getModified(srcFile);
+  resLib.updateResource(res);
+  resLib.serialize('resources.ini');
+end;
+
+procedure convertPNG(srcPath, dstPath: string);
+var
+  img: tPage;
+begin
+
+  if not preProcess(srcPath, dstPath) then exit;
+
+  img := tPage.Load(srcPath);
+
+  {better compression if we convert}
+  img.convertColorSpace(csRUV);
+
+  saveLC96(dstPath, img);
+  textAttr := LightGreen;
+  writeLn(format('[%dx%d]',[img.width, img.height]));
+  textAttr := White;
+  img.free();
+
+  postProcess(srcPath, dstPath);
+end;
+
+procedure convertWave(srcPath, dstPath: string);
+var
+  writer: tLA96Writer;
+  sfx: tSound;
+begin
+
+  if not preProcess(srcPath, dstPath) then exit;
+
+  sfx := tSound.Load(srcPath);
 
   writer := tLA96Writer.create();
   writer.open(dstPath);
@@ -58,88 +109,58 @@ begin
   textAttr := Green;
   writeLn(format('[%.2fs]',[sfx.length/441000]));
   textAttr := White;
-  sfx.free;
+  sfx.free();
+
+  postProcess(srcPath, dstPath);
 end;
 
-procedure masterGFX();
-begin
-  convertPNG('title');
-  convertPNG('template');
-  convertPNG('sprites');
-  convertPNG('netfont1');
-  convertPNG('netfont2');
-  convertPNG('font');
-end;
-
-{create compressed copies of master music tracks}
-procedure masterSFX();
+procedure processFolder(srcPath, dstPath: string; fileType: tFileType);
 var
-  filename: string;
-  tag: string;
-  root: string;
+  filename, tag: string;
+  srcExtension, dstExtension: string;
+  convert: tConvertProc;
 begin
-  root := 'c:\dev\masters\destruct';
-  for filename in fs.listFiles(joinPath(root, '\*.wav')) do begin
-    {only load 'short' audio'}
-    if fs.getFileSize(joinPath(root, filename)) > 128*1024 then continue;
+
+  case fileType of
+    ftPNG: begin
+      srcExtension := 'png';
+      dstExtension := 'p96';
+      convert := convertPNG;
+    end;
+    ftWAVE: begin
+      srcExtension := 'wav';
+      dstExtension := 'a96';
+      convert := convertWave;
+    end;
+    else raise ValueError('Invalid filetype');
+  end;
+
+  writeln(format('Processing %s -> %s [%s]', [srcPath, dstPath, srcExtension]));
+
+  for filename in fileSystem.listFiles(joinPath(srcPath, '\*.'+srcExtension)) do begin
     tag := removeExtension(extractFilename(filename));
-    convertWave(tag);
-  end;
-end;
-
-{create compressed copies of master music tracks}
-procedure masterMusic();
-var
-  verbose: boolean;
-  sourceFiles: array of string = ['dance1'];
-  writer: tLA96Writer;
-  filename: string;
-  srcPath, dstPath: string;
-  srcMusic: tSoundEffect;
-  outStream: tMemoryStream;
-begin
-  writeln();
-  writeln('--------------------------');
-  writeln('Compressing');
-  writeln('--------------------------');
-  LA96_ENABLE_STATS := false;
-  verbose := true;
-
-  writer := tLA96Writer.create();
-
-  for filename in sourceFiles do begin
-    srcPath := joinPath('c:\dev\masters\destruct', filename+'.wav');
-    if not fs.exists(srcPath) then begin
-      warning('File not found '+srcPath);
-      continue;
+    try
+      convert(joinPath(srcPath, filename), joinPath(dstPath, tag+'.'+dstExtension));
+    except
+      on e: sysUtils.Exception do begin
+        textAttr := Red;
+        writeln('[Error]');
+        textAttr := White;
+        warning(format('Error processing %s: %s', [tag, e.message]));
+      end;
     end;
 
-    dstPath := joinPath('res', filename+'.a96');
-    if (not FORCE) and fs.exists(dstPath) then begin
-      note('Skipping '+srcPath);
-      continue;
-    end;
-
-    {compress it}
-    writeln();
-    write(filename+': ');
-    srcMusic := tSoundEffect.Load(srcPath);
-    writer.open(dstPath);
-    writer.frameWriteHook := updateEncodeProgress();
-    writer.writeA96(srcMusic, ACP_HIGH);
   end;
-
-  writer.free();
-
-  writeln();
-  writeln('Done.');
 end;
-
 
 begin
   if (paramcount = 1) and (paramStr(1).toLower() = '--force') then
-    FORCE := true;
-  masterGFX();
-  masterSFX();
-  masterMusic();
+    resLib := tResourceLibrary.Create()
+  else
+    resLib := tResourceLibrary.CreateOrLoad('resources.ini');
+
+  processFolder(SRC_ROOT, joinPath(DST_ROOT, 'res'), ftPNG);
+  processFolder(SRC_ROOT, joinPath(DST_ROOT, 'res'), ftWAVE);
+
+  resLib.free();
 end.
