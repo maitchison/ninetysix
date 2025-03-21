4107466d379f4ed429638bab70dfe7a3bc73ca9b
Matthew Aitchison <matthewsaitchison@gmail.com>
2025-03-22 11:11:34 +1300

    start on listBox scrollable

--- mordor/uguilistbox.pas
+++ mordor/uguilistbox.pas
@@ -21,9 +21,17 @@ uses
 
 type
   tGUIListBox = class(tGuiComponent)
+  const
+    pad = 3;
   protected
     fMessages: tStrings;
+    scrollOffset: integer;
+  protected
+    procedure  onKeyPress(code: word); override;
+    function   rowHeight(): integer;
+    function   maxRows(): integer;
   public
+    procedure  scroll(offset: integer);
     procedure  addMessage(s: string); overload;
     procedure  addMessage(s: string; args: array of const); overload;
     procedure  doDraw(const dc: tDrawContext); override;
@@ -43,28 +51,45 @@ begin
   addMessage(format(s, args));
 end;
 
+procedure tGUIListBox.onKeyPress(code: word);
+begin
+  case code of
+    key_openSquareBracket: scroll(-1);
+    key_closeSquareBracket: scroll(+1);
+  end;
+end;
+
+function tGuiListBox.rowHeight(): integer;
+begin
+  result := font.height+1;
+end;
+
+function tGuiListBox.maxRows(): integer;
+begin
+  maxRows := floor((bounds.height-2*pad)/rowHeight);
+end;
+
+procedure tGUIListBox.scroll(offset: integer);
+begin
+  scrollOffset += offset;
+  if scrollOffset < 0 then scrollOffset := 0;
+  isDirty := true;
+end;
+
 procedure tGUIListBox.doDraw(const dc: tDrawContext);
 var
   messageIdx: integer;
   row: integer;
-  rowHeight: integer;
-  maxRows: integer;
   fontHeight: integer;
-const
-  pad: integer = 3;
 begin
   inherited doDraw(dc);
 
   dc.fillRect(bounds, RGBA.Black);
   dc.drawRect(bounds, RGB(rnd,rnd,rnd));
 
-
-  rowHeight := font.height+1;
-  maxRows := floor((bounds.height-2*pad)/rowHeight);
-
   {display messages}
   for row := 0 to maxRows-1 do begin
-    messageIdx := length(fMessages)-1-row;
+    messageIdx := length(fMessages)-1-row+scrollOffset;
     if messageIdx < 0 then break;
     font.textOut(dc, 1+pad, pad+row*rowHeight, fMessages[messageIdx], RGBA.White);
 
